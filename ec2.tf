204ef77a41dedf80c4284abb4b5b1b840069d561
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-17 05:32:41 +0530

    Upating jenkins module

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,20 @@
 resource "aws_instance" "jenkins" {
-  ami           = ""
-  instance_type = ""
+  ami           = data.aws_ami.devops_image.id
+  instance_type = "t2.micro"
   tags          = {
     Name        = "jenkins"
+  }
+}
+
+resource "null_resource" "apply" {
+  provisioner "remote-exec" {
+    connection {
+      host      = aws_instance.jenkins.private_ip
+      user      = "root"
+      password  = "DevOps321"
+      timeout   = "5m"
+    }
+    inline = [
+    "curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker-stack/install.sh| bash"]
   }
 }
