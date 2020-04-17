e35d7c11b5e93507c8713f9dd1f58ef7e01a4f87
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-17 05:40:15 +0530

    Upating jenkins module

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "jenkins" {
   ami           = data.aws_ami.devops_image.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_jenkins.id]
   tags          = {
     Name        = "jenkins"
   }
