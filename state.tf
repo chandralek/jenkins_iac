c9dd2f7fb5193968f5b79cddb1cbd483cc0dd7d9
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-18 14:21:43 +0530

    Upating jenkins module

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkinsbucket2"
+    bucket = "tfstaterobo"
     key    = "jenkins/terraform.tfstate"
     region = "us-west-2"
     dynamodb_table = "terraform-locking"
