3e4aa1b821371d36245300587403897bc43edf28
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-18 14:36:58 +0530

    Upating jenkins module

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -8,6 +8,8 @@ fi
 mkdir  -p /var/lib/jenkins
 mount /dev/xvdh /var/lib/jenkins
 
+yum install java -y
+
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install jenkins -y
